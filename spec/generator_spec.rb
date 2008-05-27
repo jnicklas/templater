307e59b9d10249e7f69c308af388399199ed9752
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-05-27 23:17:48 +0200

    refactored generator specs for :as => :hash

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -135,12 +135,11 @@ describe Templater::Generator, '.argument as hash' do
 
   before do
     @generator_class = Class.new(Templater::Generator)
+    @generator_class.argument(0, :monkey)
+    @generator_class.argument(1, :llama, :as => :hash)
   end
   
   it "should allow assignment of hashes" do
-    @generator_class.argument(0, :monkey)
-    @generator_class.argument(1, :llama, :as => :hash)
-    
     instance = @generator_class.new('/tmp', {}, 'a monkey', { :hash => 'blah' })
     
     instance.monkey.should == 'a monkey'
@@ -151,17 +150,11 @@ describe Templater::Generator, '.argument as hash' do
   end
   
   it "should convert a key/value pair to a hash" do
-    @generator_class.argument(0, :monkey)
-    @generator_class.argument(1, :llama, :as => :hash)
-    
     instance = @generator_class.new('/tmp', {}, 'a monkey', 'test:unit')
     instance.llama['test'].should == 'unit'
   end
   
   it "should consume the remaining arguments and convert them to a hash if they are key/value pairs" do
-    @generator_class.argument(0, :monkey)
-    @generator_class.argument(1, :llama, :as => :hash)
-    
     instance = @generator_class.new('/tmp', {}, 'a monkey', 'test:unit', 'john:silver', 'river:road')
     instance.llama['test'].should == 'unit'
     instance.llama['john'].should == 'silver'
@@ -169,16 +162,10 @@ describe Templater::Generator, '.argument as hash' do
   end
   
   it "should raise an error if one of the remaining arguments is not a key/value pair" do
-    @generator_class.argument(0, :monkey)
-    @generator_class.argument(1, :llama, :as => :hash)
-    
     lambda { @generator_class.new('/tmp', {}, 'a monkey', 'a:llama', 'duck:llama', 'not_a_pair', 'pair:blah') }.should raise_error(Templater::MalformattedArgumentError)
   end
   
   it "should raise error if the argument is neither a hash nor a key/value pair" do
-    @generator_class.argument(0, :monkey)
-    @generator_class.argument(1, :llama, :as => :hash)
-    
     lambda { @generator_class.new('/tmp', {}, 'a monkey', 23) }.should raise_error(Templater::MalformattedArgumentError)
     instance = @generator_class.new('/tmp')
     lambda { instance.llama = :not_a_hash }.should raise_error(Templater::MalformattedArgumentError)
