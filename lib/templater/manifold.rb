f7c67ef6e235a70b9ad60848ea975846019a0e14
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-06-27 11:01:50 +0200

    docs for template and manifold

--- lib/templater/manifold.rb
+++ lib/templater/manifold.rb
@@ -4,16 +4,25 @@ module Templater
     
     attr_accessor :generators
     
+    # Add a generator to this manifold
+    # 
+    # === Parameters
+    # name<Symbol>:: The name given to this generator in the manifold
+    # generator<Templater::Generator>:: The generator class
     def add(name, generator)
       @generators ||={}
       @generators[name.to_sym] = generator
       (class << self; self; end).module_eval <<-MODULE
         def #{name}
-          @generators[:#{name}]
+          generator(:#{name})
         end
       MODULE
     end
     
+    # Remove the generator with the given name from the manifold
+    #
+    # === Parameters
+    # name<Symbol>:: The name of the generator to be removed.
     def remove(name)
       @generators.delete(name.to_sym)
       (class << self; self; end).module_eval <<-MODULE
@@ -21,15 +30,37 @@ module Templater
       MODULE
     end
     
+    # Finds the class of a generator, given its name in the manifold.
+    #
+    # === Parameters
+    # name<Symbol>:: The name of the generator to find
+    #
+    # === Returns
+    # Templater::Generator:: The found generator class
     def generator(name)
       @generators ||= {}
       @generators[name.to_sym]
     end
     
+    # A Shortcut method for invoking the command line interface provided with Templater.
+    #
+    # === Parameters
+    # destination_root<String>:: Where the generated files should be put, this would usually be Dir.pwd
+    # name<String>:: The name of the executable running this generator (such as 'merb-gen')
+    # version<String>:: The version number of the executable.
+    # args<Array[String]>:: An array of arguments to pass into the generator. This would usually be ARGV
     def run_cli(destination_root, name, version, args)
       Templater::CLI::Manifold.run(destination_root, self, name, version, args)
     end
     
+    # If the argument is omitted, simply returns the description for this manifold, otherwise
+    # sets the description to the passed string.
+    #
+    # === Parameters
+    # text<String>:: A description
+    #
+    # === Returns
+    # String:: The description for this manifold
     def desc(text = nil)
       @text = text if text
       return @text.realign_indentation
--- lib/templater/template.rb
+++ lib/templater/template.rb
@@ -3,6 +3,15 @@ module Templater
   
     attr_accessor :context, :name, :source, :destination, :options
   
+    # Builds a new template, given the context (e.g. binding) in which the template will be rendered
+    # (usually a generator), the name of the template and its source and destination.
+    #
+    # === Parameters
+    # context<Object>:: Context for rendering
+    # name<Symbol>:: The name of this template
+    # source<String>:: Full path to the source of this template
+    # destination<String>:: Full path to the destination of this template
+    # render<Boolean>:: If set to false, will do a copy instead of rendering.
     def initialize(context, name, source, destination, render = true)
       @context = context
       @name = name
@@ -11,26 +20,40 @@ module Templater
       @options = { :render => render }
     end
     
+    # Returns the destination path relative to Dir.pwd. This is useful for prettier output in interfaces
+    # where the destination root is Dir.pwd.
+    #
+    # === Returns
+    # String:: The destination relative to Dir.pwd
     def relative_destination
       @destination.sub(Dir.pwd + '/', '')
     end
   
-    # Renders the template, and returns the result as a string
+    # Renders the template using ERB and returns the result as a String.
+    #
+    # === Returns
+    # String:: The rendered template.
     def render
       ERB.new(File.read(source), nil, '-').result(context.send(:binding))
     end
 
-    # returns true if the destination file exists.
+    # Checks if the destination file already exists.
+    #
+    # === Returns
+    # Boolean:: true if the file exists, false otherwise.
     def exists?
       File.exists?(destination)
     end
   
-    # returns true if the content of the file at the destination is identical to the rendered result.
+    # Checks if the content of the file at the destination is identical to the rendered result.
+    # 
+    # === Returns
+    # Boolean:: true if it is identical, false otherwise.
     def identical?
       File.read(destination) == render if File.exists?(destination)
     end
   
-    # Renders the template and copies it to the destination
+    # Renders the template and copies it to the destination.
     def invoke!
       FileUtils.mkdir_p(File.dirname(destination))
       if options[:render]
