0084584ccfd6178a7458c1db2674bbd4375c1f63
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-07-16 22:58:39 +0200

    refixed that generator spec typo :P

--- spec/generator/desc_spec.rb
+++ spec/generator/desc_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Templater::Generator, '#desc' do
+describe Templater::Generator, '.desc' do
   it "should append text when called with an argument, and return it when called with no argument" do
     @generator_class = Class.new(Templater::Generator)
     
