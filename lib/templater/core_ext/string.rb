4e6de487b590c5cfb5e67b8c8d59b4360b38fefd
Theo Hultberg and Jonas Nicklas <dev+iconara+jnicklas@elabs.se>
2009-08-27 23:40:50 +0200

    Finally a solid to_lines implementation?

--- lib/templater/core_ext/string.rb
+++ lib/templater/core_ext/string.rb
@@ -5,18 +5,13 @@ class String
     return self.to_lines.map { |s| s[basis..-1] }.join
   end
 
-  # Provides backward compatible way to get a collection of lines.  To avoid
-  # overriding of the original each_line of ruby-1.8.6, we define new method
-  # for that purpose.  Since defining to_a for ruby-1.9.1 is also bad idea,
-  # even if it were necessary, it should not be to_a but such like to_lines
-  # which is as follows.
-  if RUBY_VERSION <= "1.8.6"
+  if "".respond_to?(:lines)
     def to_lines
-      to_a
+      lines.to_a
     end
   else
     def to_lines
-      each_line.to_a
+      to_a
     end
   end
 end
